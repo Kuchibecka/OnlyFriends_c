fd576a41663c93c6401865b7b50d24bcbb7fa793
kuchibecka <zverev827@gmail.com>
2023-03-23 19:25:01 +0300

    Introducing a new PostList component

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -4,7 +4,7 @@
       <div class="col">
         <q-input
           bottom-slots
-          v-model="newPostData"
+          v-model="newPostText"
           placeholder="Compose new post..."
           class="new-post"
           counter
@@ -24,7 +24,7 @@
         <q-btn
           @click="addNewPost()"
           class="q-mb-lg"
-          :disable="!newPostData"
+          :disable="!newPostText"
           unelevated
           rounded
           color="primary"
@@ -34,175 +34,40 @@
     </div>
     <q-separator class="post-separator" />
 
-    <q-list separator>
-      <transition-group
-        appear
-        enter-active-class="animated fadeInDown"
-        leave-active-class="animated fadeOutDown"
-      >
-        <q-item class="q-py-md" v-for="post in posts" :key="post.id">
-          <q-item-section avatar top>
-            <q-avatar size="xl">
-              <!-- todo: заменить аватар и содержание поста            -->
-              <img src="https://cdn.quasar.dev/img/avatar2.jpg" />
-            </q-avatar>
-          </q-item-section>
-          <q-item-section>
-            <q-item-label class="text-subtitle1">
-              <strong>Author's name</strong>
-              <span class="text-grey-7 q-ml-xs"> @author_tag </span>
-              <br class="lt-md" />
-              &bull; {{ howLongAgo(post.date) }}
-            </q-item-label>
-            <q-item-label class="post-text text-body1 q-pt-xs">
-              {{ post.text }}
-            </q-item-label>
-            <div class="row justify-between q-mt-md">
-              <q-btn flat round color="grey" size="sm" icon="favorite_border" />
-              <q-btn flat round color="grey" size="sm" icon="reply" />
-              <q-btn flat round color="grey" size="sm" icon="comment" />
-              <q-btn
-                flat
-                round
-                color="grey"
-                size="sm"
-                icon="delete"
-                @click="deletePost(post)"
-              />
-            </div>
-          </q-item-section>
-        </q-item>
-      </transition-group>
-    </q-list>
-  </q-page>
+<!--    <KeepAlive>-->
+    <PostList v-model="posts"/>
+<!--    </KeepAlive>-->
 
-  <q-btn @click="looog">
-    console.log
-  </q-btn>
+
+
+  </q-page>
 </template>
 
-<!--<script>
-import { formatDistance } from "date-fns";
-import { defineComponent } from "vue";
-import db from "src/boot/firebase";
-import {
-  collection,
-  query,
-  onSnapshot,
-  orderBy,
-  addDoc,
-  deleteDoc,
-  doc
-} from "firebase/firestore";
-
-export default defineComponent({
-  name: "PageHome",
-  data() {
-    return {
-      newPostData: "",
-      posts: [],
-    };
-  },
-  methods: {
-    howLongAgo(date) {
-      // docs: https://date-fns.org/v2.29.3/docs/Getting-Started
-      return formatDistance(Number(date), new Date(), { addSuffix: true });
-    },
-    async addNewPost() {
-      let newPost = {
-        text: this.newPostData,
-        date: Date.now(),
-      };
-      const docRef = await addDoc(collection(db, "posts"), newPost);
-      console.log(docRef.id);
-
-      // this.posts.unshift(newPost);
-      this.newPostData = "";
-    },
-    async deletePost(post) {
-      // console.log('Delete post: ', post)
-      // todo: исправить на идентификатор поста
-      await deleteDoc(doc(db, "posts", post.id));
-    },
-  },
-  mounted() {
-    const q = query(collection(db, "posts"), orderBy("date"));
-    const unsubscribe = onSnapshot(q, (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        let postChange = change.doc.data();
-        postChange.id = change.doc.id;
-        if (change.type === "added") {
-          // console.log("New post: ", postChange);
-          this.posts.unshift(postChange);
-        }
-        if (change.type === "modified") {
-          console.log("Modified post: ", postChange);
-        }
-        if (change.type === "removed") {
-          // console.log("Removed post: ", postChange);
-          let index = this.posts.findIndex(post => post.id === postChange.id);
-          this.posts.splice(index, 1);
-        }
-      });
-    });
-
-    // eslint-disable-next-line no-constant-condition
-    if (false) {
-      unsubscribe();
-    }
-  },
-});
-</script>-->
-
 <script setup>
-import { formatDistance } from "date-fns";
-import { onMounted, ref } from "vue";
+import {onMounted, ref} from "vue";
 import db from "src/boot/firebase";
-import {
-  collection,
-  query,
-  onSnapshot,
-  orderBy,
-  addDoc,
-  deleteDoc,
-  doc
-} from "firebase/firestore";
+import {addDoc, collection, onSnapshot, orderBy, query} from "firebase/firestore";
+import PostList from "components/HomePage/PostList.vue";
 
-// eslint-disable-next-line no-unused-vars
-const name = "PageHome";
-let newPostData = ref('')
-let posts = ref([])
+let newPostText = ref("");
+let posts = ref([]);
 
-function howLongAgo(date) {
-  // docs: https://date-fns.org/v2.29.3/docs/Getting-Started
-  return formatDistance(Number(date), new Date(), { addSuffix: true });
-}
+
 
 async function addNewPost() {
+  // let newPost DocumentData = new DocumentData();
   let newPost = {
-    text: newPostData.value,
+    text: newPostText.value,
     date: Date.now(),
+    liked: false
   };
   const docRef = await addDoc(collection(db, "posts"), newPost);
   console.log(docRef.id);
 
-  newPostData.value = "";
-}
-
-async function deletePost(post) {
-  // console.log('Delete post: ', post)
-  // todo: исправить на идентификатор поста
-  await deleteDoc(doc(db, "posts", post.id));
-}
-
-function looog() {
-  console.log("posts: ", posts.value);
-  console.log("newPostData: ", newPostData.value);
+  newPostText.value = "";
 }
 
 onMounted(() => {
-  console.log("posts: ", posts.value);
-  console.log("newPostData: ", newPostData.value);
   const q = query(collection(db, "posts"), orderBy("date"));
   // eslint-disable-next-line no-unused-vars
   const unsubscribe = onSnapshot(q, (snapshot) => {
@@ -210,21 +75,22 @@ onMounted(() => {
       let postChange = change.doc.data();
       postChange.id = change.doc.id;
       if (change.type === "added") {
-        console.log("New post: ", postChange);
+        // console.log("New post: ", postChange);
         posts.value.unshift(postChange);
       }
       if (change.type === "modified") {
-        console.log("Modified post: ", postChange);
+        let index = posts.value.findIndex((post) => post.id === postChange.id);
+        // console.log("posts[index]: ", posts.value[index]);
+        Object.assign(posts.value[index], postChange);
       }
       if (change.type === "removed") {
         // console.log("Removed post: ", postChange);
-        let index = posts.value.findIndex(post => post.id === postChange.id);
+        let index = posts.value.findIndex((post) => post.id === postChange.id);
         posts.value.splice(index, 1);
       }
     });
   });
-})
-
+});
 </script>
 
 <style lang="sass">
@@ -240,6 +106,4 @@ onMounted(() => {
   border-color: $grey-4
   background: $blue-1
 
-.post-text
-  white-space: pre-line
 </style>
